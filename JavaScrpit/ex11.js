035f27d597aaa2e895a0c7cad83228e7b0b8627c
Mateus <spntn.mateus@gmail.com>
2019-06-04 17:13:06 -0300

    aulas 04/06/19

---
+++ JavaScrpit/ex11.js
@@ -0,0 +1,8 @@
+idade = 65
+if (idade < 16){
+    console.log('Menor de idade')
+} else if (idade < 18 || idade > 65){
+    console.log('Voto Opcional')
+}else if (idade <= 65){
+    console.log('Voto Obrigatorio')
+}
